47c41a1b0215e2ea9e08acf57bc487e688852435
Neha Nishikant <31891939+NehaNishikant@users.noreply.github.com>
2023-06-22 14:11:21 -0400

    remove duplicate functions from run.py

--- run.py
+++ run.py
@@ -3,19 +3,6 @@ import Game
 import os
 import utils
 
-def getAorB(message, a="Y", b="N"):
-
-    invalid_input = True
-
-    while more_players_invalid:
-	print(message, " ", a, "/", b, ": ")
-        answer = input()
-    	if answer == a or answer == b:
-	    invalid_input = True
-	    print("Sorry try again.")
-    
-    return answer
-
 def run():
     print("Welcome to Blood on Clocktower")
 
