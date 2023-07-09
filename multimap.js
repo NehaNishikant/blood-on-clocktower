5732c2d5f55b06f62b8dfed453acc10ab09ecd07
NehaNishikant <31891939+NehaNishikant@users.noreply.github.com>
2023-07-09 03:19:39 -0400

    still adding column to multimap

--- multimap.js
+++ multimap.js
@@ -45,7 +45,6 @@ class Multimap {
             key_indexed_data = this.data.get(key_name)
             key = keys.get(key_name)
             key_indexed_data.set(key, key_info)
-
             key_info = new Map()
             key_info.set("keys", keys)
             key_info.set("value", value)
@@ -76,11 +75,30 @@ class Multimap {
     add_key_name(key_name, new_key_name, new_keys) {
         new_key_indexed_data = new Map()
         key_indexed_data = this.data.get(key_name)
-        for (const key of key_indexed_data.keys()){
-            
-            const value = this.get(key_name, key)
+
+        let keys = new Map()
+        for (const key of key_indexed_data.keys()){ // for each obj
             new_key = new_keys.get(key)
-            new_key_indexed_data.set(new_key, value)
+
+            // insert keys into old key infos
+            other_keys = this.get(new_key_name, new_key, true) // other keys of the object (dict)
+            for( const other_key_name of other_keys.keys() ) {
+                value_other_keys = this.data.get(other_key_name).get("keys")
+                value_other_keys.set(new_key_name, new_key)
+            }
+
+
+            // create new key name for all objs // thos is all messed up 
+            const value = this.get(key_name, key)
+            let x = new Map()
+            x.set("value", value)
+            
+            for (const other_key_name of this.data.keys()){
+                value_other_key = 
+                keys.set(other_key_name, value_other_key)
+            }
+            x.set("keys", keys)
+            new_key_indexed_data.set("key_info", x)
         }
 
         this.data.set(new_key_name, new_key_indexed_data)
@@ -92,7 +110,7 @@ class Multimap {
             const key = keys.get(key_name)
             const key_indexed_data = this.data.get(key_name)
             keys = key_indexed_data.get("keys")
-            keys.set()
+            keys.set(_, )
         }
     }
 
