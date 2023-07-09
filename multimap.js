4a9bc9f96a9a281c5c512679bcb1268dee9c64d7
NehaNishikant <31891939+NehaNishikant@users.noreply.github.com>
2023-07-09 01:04:04 -0400

    added add/remove column to multimap

--- multimap.js
+++ multimap.js
@@ -1,7 +1,8 @@
 /* Creates a map indexable by multiple keys 
  * To set, we ask for multiple keys and a value
  * keys are identified by key names so we ask for keys as a Map of key name to key
- * key name schema must be set upon construction and keys must always follow the scame
+ * key name schema must be set upon construction and keys must always follow the schema
+ * the schema can change with add/remove key name
  * for get, we take in a key (and associated key name) and by default return the value indexed by the given key. But if you set the optional boolean "get_keys" to be true, we will instead give you all the keys associated with value indexed by the given key
  */
 class Multimap{
@@ -67,5 +68,22 @@ class Multimap{
             key = keys.get(key_name)
             key_indexed_data.delete(key)
         }
+    }
+
+    add_key_name(key_name, new_key_name, new_keys){
+        new_key_indexed_data = new Map()
+        key_indexed_data = this.data.get(key_name)
+        for (const key of key_indexed_data){
+            value = this.get(key_name, key)
+            new_key = new_keys.get(key)
+            new_key_indexed_data.set(new_key, value)
+        }
+
+        this.data.set(new_key_name, new_key_indexed_data)
+    }
+
+    remove_key_name(key_name){
+        this.data.get(key_name).keys().forEach(this.data.get(key_name).remove(keys))
+        this.data.remove(key_name)
     }
 }
